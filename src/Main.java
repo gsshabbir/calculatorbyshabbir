637c43bdf9c369bc37d6800a10fc2a39a23adb95
gsshabbir <chaudharyshabbir246@gmail.com>
2023-10-02 14:02:15 +0500

    first calculator

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b=sc.nextInt();
+
+        int add (int a, int b){
+            return int c=a+ b;
+        }
+        int sub (int a, int b){
+            return int c=a- b;
+    }
+}
